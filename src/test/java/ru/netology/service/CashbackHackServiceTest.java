2bdffb1b90f597226bc8ef3f8539e56c490ef866
Egorius188 <postulgin98@mail.ru>
2024-01-19 18:00:17 +0300

    reformat code

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,22 +4,23 @@ import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     @org.junit.Test
-    public void testRemain(){
-        CashbackHackService service=new CashbackHackService();
-        int amount=1000;
-        int actual=service.remain(amount);
-        int expected=0;
-        assertEquals(expected,actual);
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
 
 
     }
+
     @org.junit.Test
-    public  void testRemainWithCashback(){
-        CashbackHackService service=new CashbackHackService();
-        int amount= 500;
-        int actual=service.remain(amount);
-        int expected=500;
-        assertEquals(expected,actual);
+    public void testRemainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        assertEquals(expected, actual);
     }
 
 }
